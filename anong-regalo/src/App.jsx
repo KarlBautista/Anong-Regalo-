14a76653006841f84ea94079ad57da3567039090
Karl <karlnino.bautista@tup.edu.ph>
2025-12-21 18:00:36 +0800

    made a header

---
+++ anong-regalo/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import './App.css'
+import Header from './components/Header'
+function App() {
+  return (
+    <div className='w-full'>
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ anong-regalo/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Logo from '../assets/logo.png'
+const Header = () => {
+  return (
+    <header className='w-full h-[70px] md:h-[100px] flex justify-center shadow-md'>
+        <div className='w-full md:w-[50%] flex justify-between px-5'>
+            <div className='flex flex-1 flex-nowrap items-center'>
+                <img src={Logo} alt="" className='shrink-0 w-[60px] h-[60px] md:w-[100px] md:h-[100px]'/>
+                <h1 className='ml-2 text-2xl md:text-3xl text-[#D32F2F] whitespace-nowrap'>Anong Regalo?</h1>
+         </div>
+          <div className='hidden w-[15%] md:flex md:items-center'>
+                <button className='text-2xl text-white border px-5 py-2 bg-[#D32F2F] cursor-pointer'>Hanap Regalo</button>
+            </div>
+        
+        </div>
+      
+    </header>
+  )
+}
+
+    export default Header
